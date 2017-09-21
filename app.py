8f00df70a6b9525ce2cd7c38c9e6bab5cbf884d4
KleinYuan <kaiwen.yuan1992@gmail.com>
2017-09-21 14:47:15 -0700

    Fix typos

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import os
 
 
 root_dir = os.getcwd() + '/jetson-inference'
-model_fp = '%s/build/aarch64/bin/network/ped-100/shanpshot_iter_70800.caffemodel' % root_dir
-prototxt_fp = '%s/build/aarch64/bin/network/ped-100/deploy.prototxt' % root_dir
+model_fp = '%s/build/aarch64/bin/networks/ped-100/snapshot_iter_70800.caffemodel' % root_dir
+prototxt_fp = '%s/build/aarch64/bin/networks/ped-100/deploy.prototxt' % root_dir
 flags = ' -- model %s -- prototxt %s' % (model_fp, prototxt_fp)
 command = '%s/build/aarch64/bin/detectnet-camera %s' % (root_dir, flags)
 args = shlex.split(command)
